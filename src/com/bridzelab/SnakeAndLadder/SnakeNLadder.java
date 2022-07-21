3f587d2611332bc943aacedf518413dfc795a9fd
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-07-21 15:24:25 +0530

    added player gets exact winning position 100

--- src/com/bridzelab/SnakeAndLadder/SnakeNLadder.java
+++ src/com/bridzelab/SnakeAndLadder/SnakeNLadder.java
@@ -7,36 +7,38 @@ package com.bridzelab.SnakeAndLadder;
 import java.util.Random;  
 public class SnakeNLadder {
 	public static void main(String[] args) {
-		int position = 0;
-        final int NO_PLAY=1;  // no of player
-        final int LADDER=2;   // ladder will award you reward
-        final int SNAKE=3;    // Snake take your life
-        int dieRolls = 0;
-        Random rand = new Random();
+		 int position = 0;
+	        final int NO_PLAY=1;  // no of player
+	        final int LADDER=2;   // ladder will award you reward
+	        final int SNAKE=3;    // Snake take your life
+	        int dieRolls = 0;
 
-        while(position<100) {
-            int dice = rand.nextInt(6) + 1;   //Random input integer
-            dieRolls += 1;
-            int options = rand.nextInt(3) + 1;
+	        Random rand = new Random();  //Random obj initializer
 
-            switch (options) {
-                case NO_PLAY: break;
-                case LADDER:
-                    if (position + dice <= 100) {
-                        position += dice;
-                    }
-                    break;
-                case SNAKE:
-                    if (position - dice >= 0) {
-                        position -= dice;
-                    } else
-                        position = 0;
-                    break;
-            }
-            System.out.println("Current position is: "+position);
-        }
-        System.out.println("Number of times die has rolled: "+dieRolls);
-        System.out.println("Player has WON!");
+	        while(position<100) {
+	            int dice = rand.nextInt(6) + 1; //Random input integer
+	            dieRolls += 1;
+	            int options = rand.nextInt(3) + 1;
 
-    }
+	            switch (options) {
+	                case NO_PLAY: break;
+	                case LADDER:
+	                    if (position + dice <= 100) {
+	                        position += dice;
+	                    }
+	                    break;
+	                case SNAKE:
+	                    if (position - dice >= 0) {
+	                        position -= dice;
+	                    } else
+	                        position = 0;
+	                    break;
+	            }
+	            System.out.println("Current position is: "+position);
+	        }
+	        System.out.println("Number of times die has rolled: "+dieRolls);
+	        System.out.println("Player has WON!");
+
+	    }
+	
 }
