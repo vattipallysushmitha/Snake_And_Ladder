392d59dcf872b938c7d995e9e615fed7d7a114c6
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-07-21 13:52:52 +0530

    added check for option there are no play ladder or snake

--- src/com/bridzelab/SnakeAndLadder/SnakeNLadder.java
+++ src/com/bridzelab/SnakeAndLadder/SnakeNLadder.java
@@ -1,22 +1,44 @@
-/* Q-2) The Player rolls the die
-        to get a number
-        between 1 to 6. - Use ((RANDOM)) to get the number between
-        1 to 6
+/* Q-3)The Player then checks for a Option. They are No Play,
+        Ladder or Snake.
+        - Use ((RANDOM)) to check for Options - In Case of No Play the player
+        stays in the same position
+        - In Case of Ladder the player moves ahead by the
+          number of position received in the die
+        -  In Case of Snake the player moves behind by the
+          number of position received in the die
 */
 
 package com.bridzelab.SnakeAndLadder;
 import java.util.Random;  // import random
 public class SnakeNLadder {
 	public static void main(String[] args) {
-        int position = 0;
-        int dieRolls = 0;
-        Random rand = new Random();  //Random object initializer
-	
-        while (position < 100) {
-            int dice = rand.nextInt(6) + 1;
-            dieRolls += 1;
-            position += dice;   //Actual position
-            System.out.println("Die number is: "+dice);
+		int position = 0;
+        final int NO_PLAY=1;
+        final int LADDER=2;
+        final int SNAKE=3;
+
+        Random rand = new Random();  // Make Random object
+
+        int dice = rand.nextInt(6) + 1;  //Input random integer
+
+        int options = rand.nextInt(3) + 1;
+
+        switch (options) {
+            case NO_PLAY:
+                break;
+            case LADDER:
+                if (position + dice <= 100) {
+                    position += dice;
+                }
+                break;
+            case SNAKE:
+                if (position - dice >= 0) {
+                    position -= dice;
+                } else
+                    position = 0;
+                break;
         }
-    }
+        System.out.println("Current position is: "+position);
+        }
+
 }
