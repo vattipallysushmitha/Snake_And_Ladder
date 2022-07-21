d6aec014b9e9d6456898f61d0cec8d5d3c0bfe1a
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-07-21 13:37:01 +0530

    added game played with single player at start position 0

---
+++ src/com/bridzelab/SnakeAndLadder/SnakeNLadder.java
@@ -0,0 +1,15 @@
+/* Q-1) Snake and Ladder game played with
+        single player at start
+        position 0 .
+*/
+
+package com.bridzelab.SnakeAndLadder;
+import java.util.Random;  // import random
+
+public class SnakeNLadder {
+	public static void main(String[] args) {
+		
+        int position = 0;    // Starting position is at 0
+        System.out.println("Snake and Ladder game played with single player at start position 0");
+    }
+}
