efc526506c7fde0220794d2d2a4ee05bc126b66f
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-07-23 11:48:47 +0530

    added winning position 100

--- src/com/bridzelab/SnakeAndLadder/SnakeNLadder.java
+++ src/com/bridzelab/SnakeAndLadder/SnakeNLadder.java
@@ -2,33 +2,43 @@ package com.bridzelab.SnakeAndLadder;
 public class SnakeNLadder 
 {
 	 public static final int startPosition = 0;
-		public static void main(String[] args) 
-		{
+	   public static final int winningPosition = 100;
+		public static void main(String[] args) {
 	      int dieRoll;
 	      int currentPosition = 0;
 	      int playerPosition = startPosition;
 			dieRoll = (int)(Math.random()*6) + 1;
-	      
+	      //int option = (int)(Math.random()*3);
 
-	      System.out.println("The die comes up " + dieRoll);
-	     
-	      int option = (int)(Math.random()*3);
-	      if(option==0)
+	      //System.out.println("The die comes up " + dieRoll);
+	      while (playerPosition != winningPosition)
 	      {
-	          System.out.println("No Play");
-	          currentPosition = startPosition;
+	          System.out.println("The die comes up " + dieRoll);
+	          currentPosition = playerPosition;
+	          int option = (int)(Math.random()*3);
+	          if(option==0)
+	          {
+	             System.out.println("No Play");
+	             playerPosition = currentPosition;
 
+
+	          }
+	          else if(option==1)
+	          {
+	             System.out.println("Got Ladder");
+	             playerPosition += dieRoll;
+	          }
+	          else if(option==2)
+	          {
+	             System.out.println("Snake attacked");
+	             playerPosition -= dieRoll;
+	             if(playerPosition <= 1)
+	             {
+	                   playerPosition = 1;
+	             }
+
+	          }
+	          System.out.println("New Player position is: " + playerPosition);
 	      }
-	      else if(option==1)
-	      {
-	          System.out.println("Got Ladder");
-	          currentPosition += dieRoll;
-	      }
-	      else if(option==2)
-	      {
-	          System.out.println("Snake attacked");
-	          currentPosition -= dieRoll;
-	      }
-	      System.out.println("New Player position is: " + currentPosition);
 		}
-	}
+	}	
