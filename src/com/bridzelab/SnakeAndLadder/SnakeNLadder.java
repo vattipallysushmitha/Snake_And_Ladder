f296c51b32a31a5f75575e7a56104f2fdb78c208
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-07-23 10:38:40 +0530

    modified the code

--- src/com/bridzelab/SnakeAndLadder/SnakeNLadder.java
+++ src/com/bridzelab/SnakeAndLadder/SnakeNLadder.java
@@ -1,22 +1,18 @@
-/* Q-2) The Player rolls the die
-        to get a number
-        between 1 to 6. - Use ((RANDOM)) to get the number between
-        1 to 6
-*/
-
 package com.bridzelab.SnakeAndLadder;
-import java.util.Random;  // import random
-public class SnakeNLadder {
-	public static void main(String[] args) {
+import java.util.Random;  
+public class SnakeNLadder 
+{
+	public static void main(String[] args)
+	{
         int position = 0;
         int dieRolls = 0;
-        Random rand = new Random();  //Random object initializer
-	
-        while (position < 100) {
+        Random rand = new Random();  
+        while (position < 100)
+        {
             int dice = rand.nextInt(6) + 1;
             dieRolls += 1;
-            position += dice;   //Actual position
-            System.out.println("Die number is: "+dice);
+            position += dice;   
+            System.out.println(" The Player rools the die to get a number between 1 to 6 and the Die number is: "+dice);
         }
     }
 }
